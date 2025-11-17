1c1486250202fcb54657c8db82c974eb63695f64
Aditya <adishelke10@gmail.com>
2025-11-17 11:51:09 +0530

    added

---
+++ src/TemperatureConvertor.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+const TemperatureConvertor = () => {
+  const [degree, setDegree] = useState("");
+  const [fh, setFh] = useState("");
+
+  const cToF = () => {
+    if (degree === "") {
+      return;
+    }
+    setFh(Number(degree) * 1.8 + 32);
+  };
+  return (
+    <div>
+      <div>
+        <h1>Celcius to Fahrenheit:</h1>
+        <input
+          placeholder="Enter °C"
+          value={degree}
+          onChange={(e) => setDegree(e.target.value)}
+        ></input>
+        <button onClick={cToF}>Convert</button>
+        {fh !== "" && (
+          <h4>
+            {degree}°C = {fh}°F
+          </h4>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TemperatureConvertor;
